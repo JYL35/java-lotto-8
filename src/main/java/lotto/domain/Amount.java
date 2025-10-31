34bb1d856b5f1f1e4cfd703919c4d82266a1652f
JYL35 <dlwodbs1223@naver.com>
2025-11-01 00:24:37 +0900

    feat(Amount): 구매 금액 정수 검증 기능 구현

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -4,11 +4,20 @@ public class Amount {
 
     public Amount(String purchaseAmount) {
         validateEmpty(purchaseAmount);
+        int parsedAmount = validateInteger(purchaseAmount);
     }
 
     private void validateEmpty(String purchaseAmount) {
         if (purchaseAmount == null || purchaseAmount.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 구매 금액은 비어있을 수 없습니다. 다시 입력해주세요.");
+        }
+    }
+
+    private int validateInteger(String purchaseAmount) {
+        try {
+            return Integer.parseInt(purchaseAmount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 정수여야 합니다. 다시 입력해주세요.");
         }
     }
 }
