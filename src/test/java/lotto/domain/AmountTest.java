c2e9f321fab86dc05fe7cd064242982a26780dce
JYL35 <dlwodbs1223@naver.com>
2025-10-31 23:19:15 +0900

    test(AmountTest): 구매 금액이 정수가 아니면 예외 발생 테스트 작성

--- src/test/java/lotto/domain/AmountTest.java
+++ src/test/java/lotto/domain/AmountTest.java
@@ -4,6 +4,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -13,6 +14,15 @@ public class AmountTest {
     @DisplayName("구매 금액이 비어있으면 예외 발생")
     @NullAndEmptySource
     void test_구매_금액_공백_또는_널값_예외발생(String amount) {
+        assertThatThrownBy(() -> new Amount(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    @ParameterizedTest
+    @DisplayName("구매 금액이 정수가 아니면 예외 발생")
+    @ValueSource(strings = {"a", "abc", "1000.5", " 9"})
+    void test_구매_금액_정수_아니면_예외발생(String amount) {
         assertThatThrownBy(() -> new Amount(amount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]");
