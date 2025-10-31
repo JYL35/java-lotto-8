a175e87f473b1e5e6e4ed52f6b2a09539d8226c7
JYL35 <dlwodbs1223@naver.com>
2025-11-01 00:55:49 +0900

    feat(Amount): 구매 금액 범위 검증 기능 구현

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -1,10 +1,13 @@
 package lotto.domain;
 
 public class Amount {
+    private static final int MIN_PURCHASE_AMOUNT = 1000;
+    private static final int MAX_PURCHASE_AMOUNT = 100000;
 
     public Amount(String purchaseAmount) {
         validateEmpty(purchaseAmount);
         int parsedAmount = validateInteger(purchaseAmount);
+        validateRange(parsedAmount);
     }
 
     private void validateEmpty(String purchaseAmount) {
@@ -18,6 +21,12 @@ public class Amount {
             return Integer.parseInt(purchaseAmount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 구매 금액은 정수여야 합니다. 다시 입력해주세요.");
+        }
+    }
+
+    private void validateRange(int purchaseAmount) {
+        if (purchaseAmount < MIN_PURCHASE_AMOUNT || purchaseAmount > MAX_PURCHASE_AMOUNT) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1천원 이상, 10만원 미만이어야 합니다. 다시 입력해주세요.");
         }
     }
 }
