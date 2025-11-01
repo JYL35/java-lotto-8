7ca525ffdca6c1a30b419bf55fd9141860ef0ec0
JYL35 <dlwodbs1223@naver.com>
2025-11-01 22:18:47 +0900

    feat(LottoNumber): 로또 번호 범위 검증 기능 구현

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+public class LottoNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    private final int lottoNum;
+
+    public LottoNumber(int lottoNum) {
+        validateRange(lottoNum);
+        this.lottoNum = lottoNum;
+    }
+
+    private void validateRange(int lottoNum) {
+        if (lottoNum < MIN_NUMBER || lottoNum > MAX_NUMBER) {
+            throw new IllegalArgumentException(String.format(
+                    "[ERROR] 번호는 %,d 이상, %,d 이하여야 됩니다. 다시 입력해주세요.",
+                    MIN_NUMBER, MAX_NUMBER
+            ));
+        }
+    }
+}
