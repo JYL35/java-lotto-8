b9ec4feca43e850216ac0dc82d124a5bfed17a9d
JYL35 <dlwodbs1223@naver.com>
2025-11-02 01:00:03 +0900

    test(LottoTest): 로또 번호 오름차순 정렬 테스트 작성

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,13 +1,14 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -39,5 +40,21 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]")
                 .hasMessageContaining("중복");
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또 번호는 오름차순으로 정렬된다.")
+    @CsvSource(value = {"1,42,30,45,21,17:1,17,21,30,42,45",
+            "45,40,35,30,25,20:20,25,30,35,40,45"},
+            delimiter = ':')
+    void Lotto_ToString_ReturnsSortedString(String input, String expected) {
+        List<LottoNumber> numbers = Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .toList();
+
+        Lotto lotto = new Lotto(numbers);
+
+        assertThat(lotto.toString()).isEqualTo(expected);
     }
 }
