1f776bc4f64df050df7c2c7c0a88d33a4fbb74d6
JYL35 <dlwodbs1223@naver.com>
2025-11-03 01:05:02 +0900

    feat(InputView): 사용자 입력 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,26 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public String readPurchaseAmount() {
+        return userInput(INPUT_PURCHASE_AMOUNT);
+    }
+
+    public String readWinningNumbers() {
+        return userInput(INPUT_WINNING_NUMBERS);
+    }
+
+    public String readBonusNumber() {
+        return userInput(INPUT_BONUS_NUMBER);
+    }
+
+    private String userInput(String guidePhrase) {
+        System.out.println(guidePhrase);
+        return Console.readLine();
+    }
+}
