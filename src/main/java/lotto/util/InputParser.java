440b84cea877bb90825951a75f6355758fcda448
JYL35 <dlwodbs1223@naver.com>
2025-11-02 17:36:50 +0900

    feat(InputParser): 당첨 번호 구분자 검증과 파싱 기능 구현

--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -1,10 +1,27 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputParser {
 
     public int parseAmount(String inputAmount) {
         validateEmpty(inputAmount, ErrorMessage.AMOUNT_BLANK);
         return validateInteger(inputAmount, ErrorMessage.AMOUNT_NOT_INTEGER);
+    }
+
+    public List<Integer> parseWinningNumbers(String inputWinningNumbers) {
+        validateSeparator(inputWinningNumbers, ErrorMessage.WINNING_NUMBER_ENDS_WITH_SEPARATOR);
+
+        return Arrays.stream(inputWinningNumbers.split(","))
+                .map(numberStr -> validateInteger(numberStr, ErrorMessage.WINNING_NOT_INTEGER))
+                .toList();
+    }
+
+    private void validateSeparator(String input, ErrorMessage error) {
+        if (input.endsWith(",")) {
+            throw new IllegalArgumentException(error.getMessage());
+        }
     }
 
     private void validateEmpty(String input, ErrorMessage error) {
