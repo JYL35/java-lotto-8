ff39e92696228d4133a809137c6839e46b9e4be1
JYL35 <dlwodbs1223@naver.com>
2025-11-02 16:05:16 +0900

    feat(LottoGenerator): 생성한 로또를 Lottos로 반환하는 기능 구현

--- src/main/java/lotto/service/LottoGenerator.java
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -2,13 +2,22 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Stream;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
+import lotto.domain.Lottos;
 
 public class LottoGenerator {
     private static final int LOTTO_START_NUMBER = 1;
     private static final int LOTTO_LAST_NUMBER = 45;
     private static final int LOTTO_NUMBER_COUNT = 6;
+
+    public Lottos generate(int count) {
+        List<Lotto> lottos = Stream.generate(this::createLotto)
+                .limit(count)
+                .toList();
+        return new Lottos(lottos);
+    }
 
     private Lotto createLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER,
--- src/test/java/lotto/service/LottoGeneratorTest.java
+++ src/test/java/lotto/service/LottoGeneratorTest.java
@@ -1,7 +1,8 @@
-package lotto.servcie;
+package lotto.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import lotto.domain.Lottos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
