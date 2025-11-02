1a7060b0bb8b573dcd554b41de06879113bedead
JYL35 <dlwodbs1223@naver.com>
2025-11-02 22:31:32 +0900

    test(WinningLottoTest): 로또 랭킹 반환 테스트 작성

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -44,5 +45,41 @@ public class WinningLottoTest {
         LottoNumber bonusNumber = new LottoNumber(number);
 
         assertDoesNotThrow(() -> new WinningLotto(winningLotto, bonusNumber));
+    }
+
+    @ParameterizedTest
+    @DisplayName("일치 개수와 보너스 여부에 따라 올바른 순위를 반환한다.")
+    @CsvSource(value = {
+            "1,2,3,4,5,6:FIRST",
+            "1,2,3,4,5,7:SECOND",
+            "1,2,3,4,5,8:THIRD",
+            "1,2,3,4,7,8:FOURTH",
+            "1,2,3,4,8,9:FOURTH",
+            "1,2,3,7,8,9:FIFTH",
+            "1,2,3,8,9,10:FIFTH",
+            "1,2,7,8,9,10:MISS",
+            "1,2,8,9,10,11:MISS",
+            "1,7,8,9,10,11:MISS",
+            "1,8,9,10,11,12:MISS",
+            "7,8,9,10,11,12:MISS",
+            "8,9,10,11,12,13:MISS"
+    }, delimiter = ':')
+    void WinningLotto_MatchRank_ReturnsCorrectRank(String lottoNumbers, Rank expectedRank) {
+        Lotto winningNumbers = new Lotto(List.of(
+                new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6)
+        ));
+        LottoNumber bonusNumber = new LottoNumber(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+        List<LottoNumber> lottoNums = Arrays.stream(lottoNumbers.split(","))
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .toList();
+        Lotto userLotto = new Lotto(lottoNums);
+
+        Rank rank = winningLotto.matchLank(userLotto);
+
+        assertThat(rank).isEqualTo(expectedRank);
     }
 }
