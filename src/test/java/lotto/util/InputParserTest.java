d298248af0ebdd2d18fd5863f8a736616998bb59
JYL35 <dlwodbs1223@naver.com>
2025-11-02 17:41:03 +0900

    refactor(InputParsertest): 에러 메시지에 맞게 수정

--- src/test/java/lotto/util/InputParserTest.java
+++ src/test/java/lotto/util/InputParserTest.java
@@ -27,17 +27,17 @@ public class InputParserTest {
         assertThatThrownBy(() -> inputParser.parseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]")
-                .hasMessageContaining("비어있을 수 없습니다");
+                .hasMessageContaining("구매 금액은 비어있을 수 없습니다");
     }
 
     @ParameterizedTest
     @DisplayName("구매 금액이 정수가 아니면 예외가 발생한다.")
-    @ValueSource(strings = {"a", "abc", "1000.5", " 9"})
+    @ValueSource(strings = {"a", "abc", "1000.5"})
     void parseAmount_IsNotInteger_ThrowException(String input) {
         assertThatThrownBy(() -> inputParser.parseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]")
-                .hasMessageContaining("정수여야 합니다");
+                .hasMessageContaining("구매 금액은 정수여야 합니다");
     }
 
     @ParameterizedTest
@@ -54,20 +54,22 @@ public class InputParserTest {
     }
 
     @ParameterizedTest
-    @DisplayName("당첨 번호 문자열 맨 뒤에 쉼표가 있을 경우 예외가 발생한다")
+    @DisplayName("당첨 번호 문자열 맨 뒤에 쉼표가 있을 경우 예외가 발생한다.")
     @ValueSource(strings = {"45,40,35,30,25,20,", "1,2,3,4,5,6,"})
     void parseWinningNumbers_SeparatorAtTheEnd_ThrowException(String input) {
         assertThatThrownBy(() -> inputParser.parseWinningNumbers(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("쉼표(,)로 끝날 수 없습니다");
+                .hasMessageStartingWith("[ERROR]")
+                .hasMessageContaining("당첨 번호는 쉼표(,)로 끝날 수 없습니다");
     }
 
     @ParameterizedTest
-    @DisplayName("당첨 번호에 숫자가 아닌 문자가 포함되면 예외가 발생한다")
+    @DisplayName("당첨 번호에 정수가 아니면 예외가 발생한다.")
     @ValueSource(strings = {"1,2,a,4,5,6", "1,2,,4,5,6", ",1,2,3,4,5"})
     void parseWinningNumbers_InvalidCharacter_ThrowException(String input) {
         assertThatThrownBy(() -> inputParser.parseWinningNumbers(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("숫자만");
+                .hasMessageStartingWith("[ERROR]")
+                .hasMessageContaining("당첨 번호는 정수여야");
     }
 }
