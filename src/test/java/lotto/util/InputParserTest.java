b24a33a529bf969ef0f06bd68fde50d80bc25377
JYL35 <dlwodbs1223@naver.com>
2025-11-02 16:17:38 +0900

    test(InputParserTest): Amount 문자열 파싱 예외 테스트 작성

---
+++ src/test/java/lotto/util/InputParserTest.java
@@ -0,0 +1,40 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.domain.Amount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class InputParserTest {
+
+    private final InputParser inputParser;
+
+    @BeforeEach
+    void CreateInputParser() {
+        inputParser = new InputParser();
+    }
+
+    @ParameterizedTest
+    @DisplayName("구매 금액이 공백이거나 널값이면 예외가 발생한다.")
+    @NullAndEmptySource
+    void Amount_IsBlank_ThrowException(String input) {
+        assertThatThrownBy(() -> inputParser.parseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]")
+                .hasMessageContaining("비어있을 수 없습니다");
+    }
+
+    @ParameterizedTest
+    @DisplayName("구매 금액이 정수가 아니면 예외가 발생한다.")
+    @ValueSource(strings = {"a", "abc", "1000.5", " 9"})
+    void Amount_IsNotInteger_ThrowException(String input) {
+        assertThatThrownBy(() -> inputParser.parseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]")
+                .hasMessageContaining("정수여야 합니다");
+    }
+}
