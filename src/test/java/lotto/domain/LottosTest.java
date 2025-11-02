dba79151ee5761fdbca64add838c88cf393cd882
JYL35 <dlwodbs1223@naver.com>
2025-11-02 23:13:08 +0900

    test(LottosTest): 전체 로또 당첨 통계 반환 테스트 작성

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,51 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottosTest {
+
+    @Test
+    @DisplayName("구매한 로또들의 순위를 판별하고 당첨 통계를 계산한다.")
+    void Lottos_CalculateWinningStatistics_ReturnsLottoResult() {
+        Lotto winningNumbers = new Lotto(List.of(
+                new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6)
+        ));
+        LottoNumber bonusNumber = new LottoNumber(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+        Lotto userLotto1 = new Lotto(List.of(
+                new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5), new LottoNumber(7)
+        ));
+        Lotto userLotto2 = new Lotto(List.of(
+                new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(7), new LottoNumber(8)
+        ));
+        Lotto userLotto3 = new Lotto(List.of(
+                new LottoNumber(7), new LottoNumber(8), new LottoNumber(9),
+                new LottoNumber(10), new LottoNumber(11), new LottoNumber(12)
+        ));
+        Lotto userLotto4 = new Lotto(List.of(
+                new LottoNumber(13), new LottoNumber(8), new LottoNumber(9),
+                new LottoNumber(10), new LottoNumber(11), new LottoNumber(12)
+        ));
+
+        Lottos myLotto = new Lottos(List.of(
+                userLotto1, userLotto2, userLotto3, userLotto4));
+
+        Map<Rank, Integer> statistics = myLottos.calculateWinningStatistics(winningLotto);
+
+        assertThat(statistics.get(Rank.FIRST)).isEqualTo(0);
+        assertThat(statistics.get(Rank.SECOND)).isEqualTo(1);
+        assertThat(statistics.get(Rank.THIRD)).isEqualTo(0);
+        assertThat(statistics.get(Rank.FOURTH)).isEqualTo(1);
+        assertThat(statistics.get(Rank.FIFTH)).isEqualTo(0);
+        assertThat(statistics.get(Rank.MISS)).isEqualTo(2);
+    }
+}
