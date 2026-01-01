fc6c38dfa7b9030d626e6e081c3ad9b9e79fc299
JYL35 <dlwodbs1223@naver.com>
2026-01-01 16:12:51 +0900

    feat: 로또 번호 도메인 구현

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.constant;
 public enum ErrorMessage {
 
     WRONG_UNITS("1,000원 단위가 아닙니다."),
-    OVER_MAXIMUM_AMOUNT("최대 금액인 100,000원을 넘겼습니다.");
+    OVER_MAXIMUM_AMOUNT("최대 금액인 100,000원을 넘겼습니다."),
+    NUMBER_OUT_OF_RANGE("번호가 1~45가 아닙니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private static final String TRY_AGAIN = " 다시 입력해주세요.";
---
+++ src/main/java/lotto/service/domain/LottoNumber.java
@@ -0,0 +1,29 @@
+package lotto.service.domain;
+
+import lotto.constant.ErrorMessage;
+
+public class LottoNumber {
+    private static final int MAXIMUM_RANGE = 45;
+    private static final int MINIMUM_RANGE = 1;
+
+    private final int lottoNumber;
+
+    public LottoNumber(int number) {
+        validate(number);
+        this.lottoNumber = number;
+    }
+
+    private void validate(int number) {
+        validateRange(number);
+    }
+
+    private void validateRange(int number) {
+        if (MINIMUM_RANGE > number || MAXIMUM_RANGE < number) {
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_OUT_OF_RANGE.getMessage());
+        }
+    }
+
+    public int getLottoNumber() {
+        return lottoNumber;
+    }
+}
