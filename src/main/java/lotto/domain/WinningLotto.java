d5426c94b45a36446b6db5b5d25d9c0cc79c326d
JYL35 <dlwodbs1223@naver.com>
2026-01-06 21:14:13 +0900

    feat: 일치 개수 및 보너스 일치 여부 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningLotto {
     private final Lotto winningLotto;
     private final int bonusNumber;
@@ -7,5 +9,16 @@ public class WinningLotto {
     public WinningLotto(Lotto winningLotto, int bonusNumber) {
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    public int calculateMatchCount(Lotto lotto) {
+        return (int) winningLotto.getNumbers()
+                .stream()
+                .filter(lotto.getNumbers()::contains)
+                .count();
+    }
+
+    public boolean matchBonusNumber(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
     }
 }
